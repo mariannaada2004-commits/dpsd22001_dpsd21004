3a0fe991e9a3577d5787f7b9f311b6f3143d2c1b
mariannaada2004-commits <marianna.ada2004@gmail.com>
2026-05-31 01:51:38 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const { Pool } = require('pg');
 
 const db = new Pool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'movies_db',
-    port: process.env.DB_PORT || 5432
+    host: process.env.DB_HOST || 'ep-flat-dawn-a26nshcl.eu-central-1.aws.neon.tech',
+    user: process.env.DB_USER || 'neondb_owner',
+    password: process.env.DB_PASSWORD || 'npg_8DeaERsuQTS4',
+    database: process.env.DB_NAME || 'neondb',
+    port: process.env.DB_PORT || 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
+// Εξάγουμε ΜΟΝΟ το db και τίποτα άλλο!
 module.exports = db;
