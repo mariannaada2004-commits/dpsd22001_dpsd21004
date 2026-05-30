038060a476a76dd7c519f6550efc480ac0402a81
mariannaada2004-commits <marianna.ada2004@gmail.com>
2026-05-31 02:12:01 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const db = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: 'postgres://neondb_owner:npg_8DeaERsuQTS4@ep-flat-dawn-a26nshcl.eu-central-1.aws.neon.tech/neondb',
     ssl: {
         rejectUnauthorized: false
     }
