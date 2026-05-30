296e138c6d5ee17b64d10d3c7c2c2081426c4229
mariannaada2004-commits <marianna.ada2004@gmail.com>
2026-05-31 02:00:35 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   user: "neondb_owner",
   host: "ep-flat-dawn-a26nshcl.eu-central-1.aws.neon.tech",
   database: "neondb",
-  password: "ep-flat-dawn-a26nshcl.eu-central-1.aws.neon.tech",
+  password: "npg_8DeaERsuQTS4",
   port: 5432 // <- tunnel port
   ssl: {
         rejectUnauthorized: false
