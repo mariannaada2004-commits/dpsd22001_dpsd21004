b1ea7e9503c6bd359cb7d74a31e3b493458bd6d7
mariannaada2004-commits <marianna.ada2004@gmail.com>
2026-05-31 02:29:24 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const db = new Pool({
     host: process.env.DB_HOST || 'ep-flat-dawn-a26nshcl.eu-central-1.aws.neon.tech',
     user: process.env.DB_USER || 'neondb_owner',
-    password: process.env.DB_PASSWORD || 'pg_8DeaERsuQTS4',
+    password: process.env.DB_PASSWORD || 'npg_8DeaERsuQTS4',
     database: process.env.DB_NAME || 'neondb',
     port: process.env.DB_PORT || 5432,
     ssl: {
