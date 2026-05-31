7821788cfdcc2cd7e37b25ef55f87b92f3b08813
mariannaada2004-commits <marianna.ada2004@gmail.com>
2026-05-31 13:37:31 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
-const db = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'neondb',
-    password: 'Αdmin', 
-    port: 5432
+const pool = new Pool({
+  host: 'ep-patient-union-alkhfoop-pooler.c-3.eu-central-1.aws.neon.tech',
+  port: 5432,
+  database: 'neondb',
+  user: 'neondb_owner',
+  password: 'npg_8DeaERsuQTS4',
+  ssl: { rejectUnauthorized: false }
 });
 
-module.exports = db;
+module.exports = pool;
